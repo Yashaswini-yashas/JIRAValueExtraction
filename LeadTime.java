c2831654cf4cae3ee4a138b51e4a61689042e37e
Yashaswini-yashas <47379497+Yashaswini-yashas@users.noreply.github.com>
2020-04-20 13:04:48 +0530

    lead time of an issue

--- LeadTime.java
+++ LeadTime.java
@@ -14,7 +14,7 @@ public class LeadTime {
 @Test
 public void leadTime() throws ParseException {
 	RestAssured.baseURI="http://localhost:8090/";
-	Response res=given().auth().preemptive().basic("YashaswiniS", "jira1@345").
+	Response res=given().auth().preemptive().basic("username", "password").
 			when().
 		    get("/rest/api/2/issue/10206").
 		    then().assertThat().
